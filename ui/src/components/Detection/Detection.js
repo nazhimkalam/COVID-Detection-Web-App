e8814d6f0552c062e9987571a6b67b734cba2daa
Nazhim <nazhimkalam@gmail.com>
2021-01-15 16:34:42 +0530

    added all the sample images and stuff now to implement the main functionality

--- ui/src/components/Detection/Detection.js
+++ ui/src/components/Detection/Detection.js
@@ -1,11 +1,30 @@
-import "./Detection.css";
+import './Detection.css';
 
 function Detection() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<div className="detection">
+			<h2>COVID-19 DETECTOR</h2>
+			<p className="detection__help">
+				<span>How it works: </span>Click the <strong>UPLOAD</strong> image button to upload the image and Click the{' '}
+				<strong>DETECT</strong> button to get the result.
+			</p>
+			<div className="detection__sampleInputs">
+				<h3>Sample Inputs</h3>
+				<div className="detection__sampleInputsImages">
+					<img src="https://www.radiologyinfo.org/gallery-items/images/chest-xray.jpg" alt="" />
+					<img src="https://www.itnonline.com/sites/default/files/Covid_lungs.jpeg" alt="" />
+					<img
+						src="https://prod-images-static.radiopaedia.org/images/157210/332aa0c67cb2e035e372c7cb3ceca2_jumbo.jpg"
+						alt=""
+					/>
+					<img
+						src="https://prod-images-static.radiopaedia.org/images/381278/5757c0787c96e99ee6f7a3cdb48ed1_jumbo.jpeg"
+						alt=""
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Detection
+export default Detection;
